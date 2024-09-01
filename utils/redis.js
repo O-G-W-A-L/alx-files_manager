cba1b7a43c7dbebc88539ff0c1be74b182ddeb4d
O-G-W-A-L <ogwalja04@gmail.com>
2024-08-31 23:29:43 -0700

    styling error fix

--- utils/redis.js
+++ utils/redis.js
@@ -15,23 +15,23 @@ class RedisClient {
     });
   }
 
-  //Checks client's connection status to the Redis server
+  // Checks client's connection status to the Redis server
   isAlive() {
     return this.isClientConnected;
   }
 
-  //Retrieves the value of a given key from redis server
+  // Retrieves the value of a given key from redis server
   async get(key) {
     return promisify(this.client.GET).bind(this.client)(key);
   }
 
-  //Stores a key and its value along with an expiration time
+  // Stores a key and its value along with an expiration time
   async set(key, value, duration) {
     await promisify(this.client.SETEX)
       .bind(this.client)(key, duration, value);
   }
 
-  //Removes the value of a given key
+  // Removes the value of a given key
   async del(key) {
     await promisify(this.client.DEL).bind(this.client)(key);
   }
